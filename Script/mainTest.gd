744a21d13c1598cf92cd8cf44765034979ad4a87
EhsanAK <ehsan.akhlaghy.mac@gmail.com>
2023-09-23 20:05:56 +0330

    add new files

--- Script/mainTest.gd
+++ Script/mainTest.gd
@@ -5,7 +5,9 @@ func _ready():
 
 	
 	
-	var a = Input_Model_Manager.new("res://3dModel/detailed_monkey/scene.gltf","res://3dModel/Desk/deskglb.glb",Vector3(-1,-1,-1),Vector3(0,5.3,-3),Vector3(4,4,4))
+	#var a = Input_Model_Manager.new("res://3dModel/detailed_monkey/scene.gltf","res://3dModel/Desk/deskglb.glb",Vector3(-1,-1,-1),Vector3(0,5.3,-3),Vector3(4,4,4))
+	var a = Input_Model_Manager.new("res://3dModel/blue-house/source/blue house.glb","res://3dModel/Desk/deskglb.glb",Vector3(-1,-1,-1),Vector3(0,5.3,-3),Vector3(4,4,4))
+	
 	add_child(a)
 	
 	#a.myconstructor("res://3dModel/detailed_monkey/scene.gltf","res://3dModel/Desk/deskglb.glb",Vector3(-1,-1,-1),Vector3(0,5.3,-3),Vector3(4,4,4))
